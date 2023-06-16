63f0c5b7b8321b2fc2316f917c95af99497bd828
Anya Trivedi <anyartrivedi@gmail.com>
2023-06-16 12:05:07 -0400

    Initial Commit

---
+++ Reason4Change!/Reason4Change_App.swift
@@ -0,0 +1,17 @@
+//
+//  Reason4Change_App.swift
+//  Reason4Change!
+//
+//  Created by Anya Trivedi on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Reason4Change_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
